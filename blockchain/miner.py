e988c83df6d1dae60cb3117f2fd4dcdc7c770e4f
Estevan <estevan.rodriguez10@yahoo.com>
2019-07-12 08:52:50 -0700

    finished miner.py

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -24,6 +24,14 @@ def proof_of_work(last_proof):
     print("Searching for next proof")
     proof = 0
     #  TODO: Your code here
+    # guess = f'{last_proof}{proof}'.encode()
+    # guess_hash = hashlib.sha256(guess).hexdigest()
+    
+    last_proof = hashlib.sha256(str(last_proof).encode()).hexdigest()
+    while valid_proof(last_proof, proof) is False:
+        proof += 1
+
+
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -36,9 +44,11 @@ def valid_proof(last_hash, proof):
 
     IE:  last_hash: ...999123456, new hash 123456888...
     """
-
+    
+    proof = hashlib.sha256(str(proof).encode()).hexdigest()
     # TODO: Your code here!
-    pass
+    
+    return last_hash[-6:] == proof[:6]
 
 
 if __name__ == '__main__':
